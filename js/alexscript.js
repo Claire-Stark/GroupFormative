84d57506440cf860320edab28534eec94e235d20
Tianqi Shao <845868300@cit3-501m-06.acg.ac.nz>
2018-08-30 14:22:57 +1200

    alex page scss

--- js/alexscript.js
+++ js/alexscript.js
@@ -1,46 +1,44 @@
 $(function(){
 	let key = 'M0VKGUySTYGPYpuMrc2jNMaWgc37lAA5';
+	let projectHTML = $('#templateProject').text();
 	let urlProjects = 'https://api.behance.net/v2/users/primoperezg/projects?client_id='+key;
-	// console.log(urlProject);
-	if('#alex'){
-		$.ajax({
+	let projectTemplate = Template7(projectHTML).compile();
+
+	$.ajax({
 			url:urlProjects,
 			dataType:'jsonp',
-			success:function(res){
-			
+			success:function(res){			
 			_(res.projects).each(function(project){
+
+				let output = projectTemplate(project);
+				$('.project-container').append(output);
 				
-				$('<li><img src="'+project.covers.original+'" alt=""> '+project.name+'</li>').appendTo('ul.alex-projects');  
 
 			});
 
 		}		
+	});
+
+	//Show individual projects
+	let singleProjectHTML = $('#templateSingleProject').text();
+	let singleProjectTemplate = Template7(singleProjectHTML).compile();
+	$('#portfolioModal1').on('show.bs.modal',function(e){
+		
+		let target = e.relatedTarget;
+		let projectid = $(target).data('projectid');
+		let urlProject = 'http://www.behance.net/v2/projects/'+projectid+'?api_key='+key;
+		$.ajax({
+			url:urlProject,
+			dataType:'jsonp',
+			success:function(res){
+				let project = res.project;
+				
+				var output = singleProjectTemplate(project);
+				$('.single-project-container').empty();
+				$('.single-project-container').append(output);
+			}
 		});
-	}
 
-	// if($('#alex-project').length > 0){
-	// 	//show a specific project
-	// 	let pageURl = new URL(document.location);
-
-	// 	let params = pageURl.searchParams;//a list of all query links
-	// 	let id = params.get('id');//retrive
-
-	// 	let urlProject = 'http://www.behance.net/v2/projects/'+id+'?api_key='+key
-	// 	$.ajax({
-	// 		url:urlProject,
-	// 		dataType:'jsonp',
-	// 		success:function(res){
-	// 			// console.log(res);
-	// 			let project = res.project;
-
-	// 			$('<h1>'+project.name+'</h1>').appendTo('.container')
-	// 			$('<p>'+project.description+'</p>').appendTo('.container')
-	// 			$('<h3>'+project.published_on+'</h3>').appendTo('.container')
-	// 			$('<img src="'+project.covers.original+'" alt="">').appendTo('.container')
-	// 		}
-	// 	});
-	// }
-
-
+	});
 
 });
